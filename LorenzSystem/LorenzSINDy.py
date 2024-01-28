e7fa427767d49d0d784cf9e4338556bdc797a8fc
WnDF <aksuk19@itu.edu.tr>
2024-01-28 19:03:26 +0300

    bug fix

--- LorenzSystem/LorenzSINDy.py
+++ LorenzSystem/LorenzSINDy.py
@@ -5,6 +5,7 @@ from pysindy.utils import lorenz, lorenz_control, enzyme
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
+import LorenzFunctions
 
 def Noisy_Data_Generation(x):
     rmse = mean_squared_error(x,
@@ -18,8 +19,9 @@ def Noisy_Data_Generation(x):
 #Define Lorenz System
 LorenzSys = LS(sigma=10.0, rho = 28.0, beta = 8.0/3.0)
 initial_conditions = [1 , 0, 0];
-t_span = (0,25)
+t_span = (0,10)
 dt = 0.002
+t_train = np.arange(t_span[0],t_span[-1],dt)
 
 #Solve Lorenz System with LorenzClass
 LorenzSys.set_initial_conditions(initial_conditions)
@@ -42,11 +44,26 @@ model.print()
 
 #Effect of Hyperparameter Lambda
 x_noisy = Noisy_Data_Generation(x_train)
+t_test_span = (0,15)
+dt = 0.002
+t_test = np.arange(t_span[0],t_span[-1],dt)
+x0_test = np.array([8,7,15]);
+x_test = solve_ivp(LorenzSys.lorenz_equations, t_test_span, x0_test, t_eval = t_test, **LorenzFunctions.integrator_keywords).y.T
 
 #Train Model with Noisy Data for different HyperParameters
-opt_noisy = ps.STLSQ(threshold = 0.7)
-model_noisy = ps.SINDy(feature_names = features_name, optimizer = opt_noisy)
-model_noisy.fit(x_noisy, t = dt)
-model_noisy.print()
+threshold_scan = np.linspace(0, 1.0, 10)
+coefs = []
+x_noisy = Noisy_Data_Generation(x_train)
+for i, threshold in enumerate(threshold_scan):
+    sparse_regression_optimizer = ps.STLSQ(threshold=threshold)
+    model = ps.SINDy(feature_names=features_name, 
+                     optimizer=sparse_regression_optimizer)
+    model.fit(x_noisy, t=dt, quiet=True)
+    coefs.append(model.coefficients())
+    
+LorenzFunctions.plot_pareto(coefs, sparse_regression_optimizer, model, 
+            threshold_scan, x_test, t_test)
 
-#Direct model training with SINy is not robust. Anathoer SINDy technics should be consider.
+#You can see the RMSE with respect to given threshold lambda. Lamba can choose with respect to 
+#RMSE value as seen in chart. However, we can also see the direct model training with SINy is not robust with noisy data. 
+#Anathoer SINDy technics should be consider.
