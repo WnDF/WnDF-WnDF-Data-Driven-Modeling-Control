41d28e23b9d125ad2a881eec19bb00e857332499
WnDF <aksuk19@itu.edu.tr>
2024-01-28 20:59:46 +0300

    future added

--- LorenzSystem/EnsembleMethod.py
+++ LorenzSystem/EnsembleMethod.py
@@ -18,7 +18,7 @@ def Noisy_Data_Generation(x):
 
 #Define Lorenz System
 LorenzSys = LS(sigma=10.0, rho = 28.0, beta = 8.0/3.0)
-initial_conditions = [1 , 0, 0];
+initial_conditions = [8, 7, 15];
 t_span = (0,10)
 dt = 0.002
 t_train = np.arange(t_span[0],t_span[-1],dt)
@@ -32,8 +32,31 @@ x_noisy = Noisy_Data_Generation(x_train)
 #LorenzSys.plot_solution(x_train)
 
 #Generate SINDy Model using LorenzClass Ouput to Estimate Lorenz System
-features_name = ["x", "y", "z"]
+feature_names = ["x", "y", "z"]
 opt = ps.STLSQ(threshold = 0.5) #Sequential Treshold Least Square Algorithm -> Threshold Defined Randomly
-model = ps.SINDy(feature_names = features_name, optimizer = opt)
+model = ps.SINDy(feature_names = feature_names, optimizer = opt)
 model.fit(x_noisy, t = dt)
 model.print()
+
+#System Output
+# (x)' = -9.343 x + 9.450 y
+# (y)' = 25.054 x + -0.932 x z
+# (z)' = -0.866 1 + -2.595 z + 0.987 x y
+# With noisy data, SINDy quite differs from original system.
+
+model.fit(x_noisy, t = dt, ensemble = True)
+mean_ensemble = np.mean(model.coef_list, axis = 0)
+std_ensemble = np.std(model.coef_list, axis = 0)
+
+model.fit(x_noisy, t=dt, library_ensemble = True)
+mean_library_ensemble = np.mean(model.coef_list, axis = 0)
+std_library_ensemble = np.std(model.coef_list, axis = 0)
+
+LorenzFunctions.plot_ensemble_results(model, mean_ensemble, 
+                                      std_ensemble, mean_library_ensemble, 
+                                      std_library_ensemble)
+
+# Ensemble method increase the roboustness of SINDy. It takes all of the trainging data and subsample it for generating different 
+# multiple models. Library_ensamble is quite similar method to increase roboutness. It is only subsample candidate future library 
+# (x, y, z for lorenz). It takes one of the feature in feature library and eliminate others and just fit the model on extracted future.
+# This is sometimes problematic due to elimination of other futures. Results can seen from plot_ensemble_results.
