7ef7c7435b2e56ff49c471fcd7c7e69e28e6742e
WnDF <aksuk19@itu.edu.tr>
2024-03-02 14:27:39 +0300

    nn structure changed and data plots for test added

--- DoublePedulum/DoublePendulum_NN.py
+++ DoublePedulum/DoublePendulum_NN.py
@@ -19,25 +19,26 @@ class DoublePendulumnNNModel(torch.nn.Module):
         self.input_test_data = 0
         self.output_train_data = 0
         self.output_test_data = 0
+        self.train_timespan_data = 0
+        self.test_timespan_data = 0
+        
+        self.prediction_dataframe = pd.DataFrame()
 
 
         # 2. Create 2 nn.Linear layers capable of handling X and y input and output shapes
-        self.model = torch.nn.Sequential(torch.nn.Linear(in_features=4, out_features=1000),
+        self.model = torch.nn.Sequential(torch.nn.Linear(in_features=4, out_features=1024),
                                         torch.nn.ReLU(),
-                                        torch.nn.Dropout(0.2),
-                                        torch.nn.Linear(in_features=1000, out_features=200),
+                                        torch.nn.Linear(in_features=1024, out_features=512),
                                         torch.nn.ReLU(),
-                                        torch.nn.Dropout(0.2),
-                                        torch.nn.Linear(in_features=200, out_features=1000),
+                                        torch.nn.Linear(in_features=512, out_features=256),
                                         torch.nn.ReLU(),
-                                        torch.nn.Dropout(0.2),
-                                        torch.nn.Linear(in_features=1000, out_features=200),
+                                        torch.nn.Linear(in_features=256, out_features=128),
                                         torch.nn.ReLU(),
-                                        torch.nn.Dropout(0.2),
-                                        torch.nn.Linear(in_features=200, out_features=50),
+                                        torch.nn.Linear(in_features=128, out_features=64),
                                         torch.nn.ReLU(),
-                                        torch.nn.Dropout(0.2),
-                                        torch.nn.Linear(in_features=50, out_features=4))
+                                        torch.nn.Linear(in_features=64, out_features=16),
+                                        torch.nn.ReLU(),
+                                        torch.nn.Linear(in_features=16, out_features=4))
         
     def DataGeneration(self, t_stop = 10, dt = 0.001, sim_step_size=2, 
                          m1 = 0.2704, m2 = 0.2056, cg1 = 0.191, cg2 = 0.1621, 
@@ -62,17 +63,22 @@ class DoublePendulumnNNModel(torch.nn.Module):
 
             df = double_pendulum.simulate(state, t_span, t_eval)
             self.dataset_dataframe = pd.concat([self.dataset_dataframe, df], axis = 0)
+        
+        self.SaveSimulationData()
 
         input_data = np.array(self.dataset_dataframe[['Theta1_dot', 'Theta2_dot', 'Omega1_dot', 'Omega2_dot']])
         output_data = np.array(self.dataset_dataframe[['Theta1', 'Theta2', 'Omega1', 'Omega2']])
+        timespan = np.array(self.dataset_dataframe[['Time']])
 
         input_data = torch.from_numpy(input_data).type(torch.float)
         output_data = torch.from_numpy(output_data).type(torch.float)
+        timespan = torch.from_numpy(timespan).type(torch.float)
 
-        self.input_train_data, self.input_test_data, self.output_train_data, self.output_test_data = train_test_split(input_data,
-                                                                              output_data,
-                                                                              test_size=0.2,
-                                                                              random_state = np.random.seed(self.RANDOM_SEED))
+        self.input_train_data, self.input_test_data, self.output_train_data, self.output_test_data, self.train_timespan_data, self.test_timespan_data = train_test_split(input_data, 
+                                                                                                                                                                        output_data, 
+                                                                                                                                                                        timespan,
+                                                                                                                                                                        test_size=0.2,
+                                                                                                                                                                        random_state = np.random.seed(self.RANDOM_SEED))
     
     def SaveSimulationData(self):
         self.dataset_dataframe.to_csv('DoublePendulumDataset.csv', encoding='utf-8')
@@ -89,7 +95,6 @@ class DoublePendulumnNNModel(torch.nn.Module):
         acc = (correct / len(y_pred)) * 100 
         return acc
 
-    
     def Train(self, epochs = 100):
         np.random.seed(self.RANDOM_SEED)
 
@@ -112,8 +117,58 @@ class DoublePendulumnNNModel(torch.nn.Module):
                 test_acc = self.AccuracyFunc(y_true = self.output_test_data,
                                             y_pred = test_preds)
             
-            if epoch % 10 == 0:
-                print(f"Epoch: {epoch} | Loss: {loss:.5f}, Acc: {accuracy:.2f}% | Test Loss: {test_loss:.5f}, Test Acc: {test_acc:.2f}%") 
+            if epoch % 100 == 0:
+                print(f"Epoch: {epoch} | Loss: {loss:.5f}, Acc: {accuracy:.2f}% | Test Loss: {test_loss:.5f}, Test Acc: {test_acc:.2f}%")
+
+        self.model.eval()
+
+        with torch.inference_mode():
+            predictions = self.model(self.input_test_data).numpy()
+            Theta1 = self.output_test_data[:,0].numpy()
+            Theta1_pred = predictions[:,0]
+            Theta2 = self.output_test_data[:,1].numpy()
+            Theta2_pred = predictions[:,1]
+            Omega1= self.output_test_data[:,2].numpy()
+            Omega1_pred = predictions[:,2]
+            Omega2 = self.output_test_data[:,3].numpy()
+            Omega2_pred = predictions[:,3]
+
+            t_span = self.test_timespan_data
+            plt.subplot(2, 2, 1)
+            plt.scatter(t_span, Theta1_pred, s=0.3)
+            plt.scatter(t_span, Theta1, s=0.3)
+            plt.title('Theta1')
+            plt.xlabel('Time')
+            plt.ylabel('Theta1-Theta1_pred')
+            plt.grid(True)
+
+            plt.subplot(2, 2, 2)
+            plt.scatter(t_span,Theta2_pred, s=0.3)
+            plt.scatter(t_span, Theta2, s=0.3)
+            plt.title('Theta2')
+            plt.xlabel('Time')
+            plt.ylabel('Theta2-Theta2_pred')
+            plt.grid(True)
+
+            plt.subplot(2, 2, 3)
+            plt.scatter(t_span,Omega1_pred, s=0.3)
+            plt.scatter(t_span, Omega1, s=0.3)
+            plt.title('Omega1')
+            plt.xlabel('Time')
+            plt.ylabel('Omega1-Omega1_pred')
+            plt.legend()
+            plt.grid(True)
+
+            plt.subplot(2, 2, 4)
+            plt.scatter(t_span,Omega2_pred, s=0.3)
+            plt.scatter(t_span, Omega2, s=0.3)
+            plt.title('Omega2')
+            plt.xlabel('Time')
+            plt.ylabel('Omega2-Omega2_pred')
+            plt.legend()
+            plt.grid(True)
+
+            plt.show()
 
     def DataPlots(self):
         pass
@@ -122,7 +177,7 @@ class DoublePendulumnNNModel(torch.nn.Module):
         return self.model(x)
 
 DpNNModel = DoublePendulumnNNModel()
-DpNNModel.DataGeneration(t_stop = 10, sim_step_size = 5)
+DpNNModel.DataGeneration(t_stop = 10, sim_step_size = 1)
 DpNNModel.LossFunction()
 DpNNModel.Optimizer(optimizer = torch.optim.Adam, lr = 0.001)
-DpNNModel.Train(epochs = 100)
+DpNNModel.Train(epochs = 10000)
