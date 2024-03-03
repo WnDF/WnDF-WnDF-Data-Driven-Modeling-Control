898a16784ad28d6a78df304c6a3b43735a0c01a8
WnDF <aksuk19@itu.edu.tr>
2024-03-03 17:31:59 +0300

    DataPlot structure modified

--- DoublePedulum/DoublePendulum_NN.py
+++ DoublePedulum/DoublePendulum_NN.py
@@ -65,7 +65,7 @@ class DoublePendulumnNNModel(torch.nn.Module):
             df = self.DpModel.simulate(state, t_span, t_eval)
             self.dataset_dataframe = pd.concat([self.dataset_dataframe, df], axis = 0)
         
-        self.SaveSimulationData(data = self.dataset_dataframe, filename = 'DoublePendulumDataset.cvs')
+        self.SaveSimulationData(data = self.dataset_dataframe, filename = './DoublePedulum/Dataset/DoublePendulumDatasetForNN.csv')
 
         input_data = np.array(self.dataset_dataframe[['Theta1_dot', 'Theta2_dot', 'Omega1_dot', 'Omega2_dot']])
         output_data = np.array(self.dataset_dataframe[['Theta1', 'Theta2', 'Omega1', 'Omega2']])
@@ -94,7 +94,7 @@ class DoublePendulumnNNModel(torch.nn.Module):
     def AccuracyFunc(self, y_true, y_pred, treshold = 0.01):
         acc_rates = []
         for i in range(0,len(y_true)):
-            error = torch.abs((y_true[i] - y_pred[i])/y_pred[i])
+            error = torch.abs((y_true[i] - y_pred[i])/y_true[i])
             correct = torch.sum(error <= treshold).item()
             acc = (correct / len(y_true)) * 100
             acc_rates.append(acc)
@@ -129,7 +129,7 @@ class DoublePendulumnNNModel(torch.nn.Module):
                                            'TrainLoss': loss, 'TestLoss': test_loss})
                 self.metricesbyepochs = pd.concat([self.metricesbyepochs, dfmetrices], axis = 0)
             
-            if epoch % 100 == 0:
+            if epoch % 10 == 0:
                 print(f"Epoch: {epoch} | Loss: {loss:.5f} | Theta1Acc: {test_acc[0]:.2f}% | Theta2Acc: {test_acc[1]:.2f}% | Test Loss: {test_loss:.5f}")
 
     def PoseByAxis(self, theta1_true, theta1_pred, theta2_true, theta2_pred):
@@ -179,170 +179,190 @@ class DoublePendulumnNNModel(torch.nn.Module):
                                     })
             
             self.prediction_dataframe = pd.concat([self.prediction_dataframe, pred_df], axis = 1)
-            self.SaveSimulationData(data = self.prediction_dataframe, filename = 'ModelEval.cvs')
+            self.SaveSimulationData(data = self.prediction_dataframe, filename = './DoublePedulum/Dataset/NNModelEval.csv')
 
     def DataPlots(self):
-            Theta1 = self.prediction_dataframe['Theta1']
-            Theta2 = self.prediction_dataframe['Theta2']
-            Omega1= self.prediction_dataframe['Omega1']
-            Omega2 = self.prediction_dataframe['Omega2']
-            x1_true = self.prediction_dataframe['x1_true']
-            y1_true = self.prediction_dataframe['y1_true']
-            x2_true = self.prediction_dataframe['x2_true']
-            y2_true = self.prediction_dataframe['y2_true']
 
-            Theta1_pred = self.prediction_dataframe['Theta1_pred']
-            Theta2_pred = self.prediction_dataframe['Theta2_pred']
-            Omega1_pred = self.prediction_dataframe['Omega1_pred']
-            Omega2_pred = self.prediction_dataframe['Omega2_pred']
-            x1_pred = self.prediction_dataframe['x1_pred']
-            y1_pred = self.prediction_dataframe['y1_pred']
-            x2_pred = self.prediction_dataframe['x2_pred']
-            y2_pred =self.prediction_dataframe['y2_pred']
+        Theta1 = self.prediction_dataframe['Theta1']
+        Theta2 = self.prediction_dataframe['Theta2']
+        Omega1= self.prediction_dataframe['Omega1']
+        Omega2 = self.prediction_dataframe['Omega2']
+        x1_true = self.prediction_dataframe['x1_true']
+        y1_true = self.prediction_dataframe['y1_true']
+        x2_true = self.prediction_dataframe['x2_true']
+        y2_true = self.prediction_dataframe['y2_true']
 
-            t_span = self.test_timespan_data
+        Theta1_pred = self.prediction_dataframe['Theta1_pred']
+        Theta2_pred = self.prediction_dataframe['Theta2_pred']
+        Omega1_pred = self.prediction_dataframe['Omega1_pred']
+        Omega2_pred = self.prediction_dataframe['Omega2_pred']
+        x1_pred = self.prediction_dataframe['x1_pred']
+        y1_pred = self.prediction_dataframe['y1_pred']
+        x2_pred = self.prediction_dataframe['x2_pred']
+        y2_pred =self.prediction_dataframe['y2_pred']
 
-            epochs = self.metricesbyepochs['Epoch']
-            TrainLoss = self.metricesbyepochs['TrainLoss']
-            TestLoss = self.metricesbyepochs['TestLoss']
-            lr = self.metricesbyepochs['Lr']
+        t_span = self.test_timespan_data
 
-            Theta1Accuracy = self.metricesbyepochs['Theta1Accuracy']
-            Theta2Accuracy = self.metricesbyepochs['Theta2Accuracy']
-            Omega1Accuracy = self.metricesbyepochs['Omega1Accuracy']
-            Omega2Accuracy = self.metricesbyepochs['Omega2Accuracy']
-            
-            plt.subplot(2, 2, 1)
-            plt.scatter(t_span, Theta1_pred, s=0.3)
-            plt.scatter(t_span, Theta1, s=0.3)
-            plt.title('Theta1')
-            plt.xlabel('Time')
-            plt.ylabel('Theta1-Theta1_pred')
-            plt.grid(True)
+        epochs = self.metricesbyepochs['Epoch']
+        TrainLoss = self.metricesbyepochs['TrainLoss']
+        TestLoss = self.metricesbyepochs['TestLoss']
+        lr = self.metricesbyepochs['Lr']
 
-            plt.subplot(2, 2, 2)
-            plt.scatter(t_span,Theta2_pred, s=0.3)
-            plt.scatter(t_span, Theta2, s=0.3)
-            plt.title('Theta2')
-            plt.xlabel('Time')
-            plt.ylabel('Theta2-Theta2_pred')
-            plt.grid(True)
+        Theta1Accuracy = self.metricesbyepochs['Theta1Accuracy']
+        Theta2Accuracy = self.metricesbyepochs['Theta2Accuracy']
+        Omega1Accuracy = self.metricesbyepochs['Omega1Accuracy']
+        Omega2Accuracy = self.metricesbyepochs['Omega2Accuracy']
+        
+        FigSize = (60,50)
+        dotsize = 5
 
-            plt.subplot(2, 2, 3)
-            plt.scatter(t_span,Omega1_pred, s=0.3)
-            plt.scatter(t_span, Omega1, s=0.3)
-            plt.title('Omega1')
-            plt.xlabel('Time')
-            plt.ylabel('Omega1-Omega1_pred')
-            plt.grid(True)
+        plt.rc('axes', titlesize=40)        # Controls Axes Title
+        plt.rc('axes', labelsize=30)        # Controls Axes Labels
+        plt.rc('xtick', labelsize=30)       # Controls x Tick Labels
+        plt.rc('ytick', labelsize=30)       # Controls y Tick Labels
+        plt.rc('legend', fontsize=30)       # Controls Legend Font
 
-            plt.subplot(2, 2, 4)
-            plt.scatter(t_span, Omega2_pred, s=0.3)
-            plt.scatter(t_span, Omega2, s=0.3)
-            plt.title('Omega2')
-            plt.xlabel('Time')
-            plt.ylabel('Omega2-Omega2_pred')
-            plt.grid(True)
+        fig1 = plt.figure(figsize = FigSize)
+        fig1ax1 = fig1.add_subplot(2,1,1)
+        fig1ax2 = fig1.add_subplot(2,1,2)
+        fig1ax1.scatter(t_span, Theta1_pred, s=dotsize, label = 'Pred.')
+        fig1ax1.scatter(t_span, Theta1, s=dotsize, label = 'True')
+        fig1ax1.set_title("First Link Angle - " r'$\theta_{1}$', fontweight="bold")
+        fig1ax1.set_xlabel('Time (sec)')
+        fig1ax1.set_ylabel(r'$\theta_{1}$'u'\xb0' " (deg)")
+        fig1ax1.legend(loc = 'upper right')
+        fig1ax1.grid(linestyle='--', linewidth=2)
+        fig1ax2.scatter(t_span,Theta2_pred, s=dotsize, label = 'Pred.')
+        fig1ax2.scatter(t_span, Theta2, s=dotsize, label = 'Pred.')
+        fig1ax2.set_title("Second Link Angle - "r'$\theta_{2}$', fontweight="bold")
+        fig1ax2.set_xlabel('Time (sec)')
+        fig1ax2.set_ylabel(r'$\theta_{2}$'u'\xb0'" (deg)")
+        fig1ax2.legend(loc = 'upper right')
+        fig1ax2.grid(linestyle='--', linewidth=2)
+        fig1.savefig("./DoublePedulum/NNFigures/Angle Predictions.png")
 
-            plt.show()
-            plt.savefig("NNFigures/Angle & Omega Predictions.png")
-            
-            plt.subplot(2, 2, 1)
-            plt.scatter(t_span, x1_true, s=0.3)
-            plt.scatter(t_span, x1_pred, s=0.3)
-            plt.title('First Link X Pose')
-            plt.xlabel('Time')
-            plt.ylabel('x')
-            plt.grid(True)
+        fig2 = plt.figure(figsize = FigSize)
+        fig2ax1 = fig2.add_subplot(2,1,1)
+        fig2ax2 = fig2.add_subplot(2,1,2)
+        fig2ax1.scatter(t_span, Omega1_pred, s=dotsize, label = 'Pred.')
+        fig2ax1.scatter(t_span, Omega1, s=dotsize, label = 'True')
+        fig2ax1.set_title("First Link Angular Speed - " r'$\omega_{1}$', fontweight="bold")
+        fig2ax1.set_xlabel('Time (sec)')
+        fig2ax1.set_ylabel(r'$\omega_{1}$'" (deg/s)")
+        fig2ax1.legend(loc = 'upper right')
+        fig2ax1.grid(linestyle='--', linewidth=2)
+        fig2ax2.scatter(t_span, Omega2_pred, s=dotsize, label = 'Pred.')
+        fig2ax2.scatter(t_span, Omega2, s=dotsize, label = 'True')
+        fig2ax2.set_title("Second Link Angular Speed - " r'$\omega_{2}$', fontweight="bold")
+        fig2ax2.set_xlabel('Time (sec)')
+        fig2ax2.set_ylabel(r'$\omega_{2}$'" (deg/s)")
+        fig2ax2.legend(loc = 'upper right')
+        fig2ax2.grid(linestyle='--', linewidth=2)
+        fig2.savefig("./DoublePedulum/NNFigures/Omega Predictions.png")
 
-            plt.subplot(2, 2, 2)
-            plt.scatter(t_span, y1_true, s=0.3)
-            plt.scatter(t_span, y1_pred, s=0.3)
-            plt.title('First Link Y Pose')
-            plt.xlabel('Time')
-            plt.ylabel('y')
-            plt.grid(True)
+        fig3 = plt.figure(figsize = FigSize)
+        fig3ax1 = fig3.add_subplot(2,1,1)
+        fig3ax2 = fig3.add_subplot(2,1,2)
+        fig3ax1.scatter(t_span, x1_true, s=dotsize, label = 'True')
+        fig3ax1.scatter(t_span, x1_pred, s=dotsize, label = 'Pred.')
+        fig3ax1.set_title('First Link Position - X Axis', fontweight="bold")
+        fig3ax1.set_xlabel('Time (sec)')
+        fig3ax1.set_ylabel('x (m)')
+        fig3ax1.grid(linestyle='--', linewidth=2)
+        fig3ax1.legend(loc = 'upper right')
+        fig3ax2.scatter(t_span, y1_true, s=dotsize, label = 'True')
+        fig3ax2.scatter(t_span, y1_pred, s=dotsize, label = 'Pred.')
+        fig3ax2.set_title('First Link Position - Y Axis', fontweight="bold")
+        fig3ax2.set_xlabel('Time (sec)')
+        fig3ax2.set_ylabel('y (m)')
+        fig3ax2.legend(loc = 'upper right')
+        fig3ax2.grid(linestyle='--', linewidth=2)
+        fig3.savefig("./DoublePedulum/NNFigures/First Link Pose.png")
 
-            plt.subplot(2, 2, 3)
-            plt.scatter(t_span, x2_true, s=0.3)
-            plt.scatter(t_span, x2_pred, s=0.3)
-            plt.title('Second Link X Pose')
-            plt.xlabel('Time')
-            plt.ylabel('x')
-            plt.grid(True)
+        fig4 = plt.figure(figsize = FigSize)
+        fig4ax1 = fig4.add_subplot(2,1,1)
+        fig4ax2 = fig4.add_subplot(2,1,2)
+        fig4ax1.scatter(t_span, x2_true, s=dotsize, label = 'True')
+        fig4ax1.scatter(t_span, x2_pred, s=dotsize, label = 'Pred.')
+        fig4ax1.set_title('Second Link Position - X Axis', fontweight="bold")
+        fig4ax1.set_xlabel('Time (sec)')
+        fig4ax1.set_ylabel('x (m)')
+        fig4ax1.legend(loc = 'upper right')
+        fig4ax1.grid(linestyle='--', linewidth=2)
+        fig4ax2.scatter(t_span, y2_true, s=dotsize, label = 'True')
+        fig4ax2.scatter(t_span, y2_pred, s=dotsize, label = 'Pred.')
+        fig4ax2.set_title('Second Link Position - Y Axis', fontweight="bold")
+        fig4ax2.set_xlabel('Time (sec)')
+        fig4ax2.set_ylabel('y (m)')
+        fig4ax2.legend(loc = 'upper right')
+        fig4ax2.grid(linestyle='--', linewidth=2)
+        fig4.savefig("./DoublePedulum/NNFigures/Second Link Pose.png")
 
-            plt.subplot(2, 2, 4)
-            plt.scatter(t_span, y2_true, s=0.3)
-            plt.scatter(t_span, y2_pred, s=0.3)
-            plt.title('Second Link Y Pose')
-            plt.xlabel('Time')
-            plt.ylabel('y')
-            plt.grid(True)
+        fig5 = plt.figure(figsize = FigSize)
+        fig5ax1 = fig5.add_subplot(3,1,1)
+        fig5ax2 = fig5.add_subplot(3,1,2)
+        fig5ax3 = fig5.add_subplot(3,1,3)
+        fig5ax1.plot(epochs, TestLoss, label = 'Test Loss')
+        fig5ax1.set_title('Test Loss by Epoch', fontweight="bold")
+        fig5ax1.set_xlabel('Epoch')
+        fig5ax1.set_ylabel('Loss')
+        fig5ax1.legend(loc = 'upper right')
+        fig5ax1.grid(linestyle='--', linewidth=2)
+        fig5ax2.plot(epochs, TrainLoss, label = 'Train Loss')
+        fig5ax2.set_title('Train Loss by Epoch', fontweight="bold")
+        fig5ax2.set_xlabel('Epoch')
+        fig5ax2.set_ylabel('Loss')
+        fig5ax2.legend(loc = 'upper right')
+        fig5ax2.grid(linestyle='--', linewidth=2)
+        fig5ax3.plot(epochs, lr, label = 'Lr')
+        fig5ax3.set_title('Learning Rate by Epoch', fontweight="bold")
+        fig5ax3.set_xlabel('Epoch')
+        fig5ax3.set_ylabel('Lr')
+        fig5ax3.legend(loc = 'upper right')
+        fig5ax3.grid(linestyle='--', linewidth=2)
+        fig5.savefig("./DoublePedulum/NNFigures/Loss & Learning Rate.png")
 
-            plt.show()
-            plt.savefig("Position of Links.png")
+        fig6 = plt.figure(figsize = FigSize)
+        fig6ax1 = fig6.add_subplot(2,1,1)
+        fig6ax2 = fig6.add_subplot(2,1,2)
+        fig6ax1.plot(epochs, Theta1Accuracy, label = 'Acc. Rate (%)')
+        fig6ax1.set_title('Accuracy Rate - 'r'$\theta_{1}$'u'\xb0' , fontweight="bold")
+        fig6ax1.set_xlabel('Epoch')
+        fig6ax1.set_ylabel('Accuracy (%)')
+        fig6ax1.legend(loc = 'upper right')
+        fig6ax1.grid(linestyle='--', linewidth=2)
+        fig6ax2.plot(epochs, Theta2Accuracy, label = 'Accuracy Rate (%)')
+        fig6ax2.set_title('Accuracy Rate - 'r'$\theta_{2}$'u'\xb0' , fontweight="bold")
+        fig6ax2.set_xlabel('Epoch')
+        fig6ax2.set_ylabel('Accuracy (%)')
+        fig6ax2.legend(loc = 'upper right')
+        fig6ax2.grid(linestyle='--', linewidth=2)
+        fig6.savefig("./DoublePedulum/NNFigures/Theta Accuracy.png")
 
-            plt.subplot(1, 3, 1)
-            plt.plot(epochs, TestLoss)
-            plt.title('Test Loss')
-            plt.xlabel('Epoch')
-            plt.ylabel('Loss')
-            plt.grid(True)
-
-            plt.subplot(1, 3, 2)
-            plt.plot(epochs, TrainLoss)
-            plt.title('Train Loss')
-            plt.xlabel('Epoch')
-            plt.ylabel('Loss')
-            plt.grid(True)
-
-            plt.subplot(1, 3, 3)
-            plt.plot(epochs, lr)
-            plt.title('Learning Rate')
-            plt.xlabel('Epoch')
-            plt.ylabel('Lr')
-            plt.grid(True)
-
-            plt.show()
-            plt.savefig("NNFigures/Loss Values & Learning Rate.png")
-
-            plt.subplot(2, 2, 1)
-            plt.scatter(epochs, Theta1Accuracy, s = 0.3)
-            plt.title('Theta1 Accuracy')
-            plt.xlabel('Epoch')
-            plt.ylabel('Accuracy')
-            plt.grid(True)
-
-            plt.subplot(2, 2, 2)
-            plt.scatter(epochs, Theta2Accuracy, s = 0.3)
-            plt.title('Theta2 Accuracy')
-            plt.xlabel('Epoch')
-            plt.ylabel('Accuracy')
-            plt.grid(True)
-
-            plt.subplot(2, 2, 3)
-            plt.scatter(epochs, Omega1Accuracy, s = 0.3)
-            plt.title('Omega1 Accuracy')
-            plt.xlabel('Epoch')
-            plt.ylabel('Accuracy')
-            plt.grid(True)
-
-            plt.subplot(2, 2, 4)
-            plt.scatter(epochs, Omega2Accuracy, s = 0.3)
-            plt.title('Omega2 Accuracy')
-            plt.xlabel('Epoch')
-            plt.ylabel('Accuracy')
-            plt.grid(True)
-
-            plt.show()
-            plt.savefig("NNFigures/Predictions Accuracy.png")
+        fig7 = plt.figure(figsize = FigSize)
+        fig7ax1 = fig7.add_subplot(2,1,1)
+        fig7ax2 = fig7.add_subplot(2,1,2)
+        fig7ax1.plot(epochs, Omega1Accuracy, label = 'Accuracy Rate (%)')
+        fig7ax1.set_title('Accuracy Rate - 'r'$\omega_{1}$'u'\xb0', fontweight="bold")
+        fig7ax1.set_xlabel('Epoch')
+        fig7ax1.set_ylabel('Accuracy (%)')
+        fig7ax1.legend(loc = 'upper right')
+        fig7ax1.grid(linestyle='--', linewidth=2)
+        fig7ax2.plot(epochs, Omega2Accuracy, label = 'Accuracy Rate (%)')
+        fig7ax2.set_title('Accuracy Rate - 'r'$\omega_{2}$'u'\xb0' , fontweight="bold")
+        fig7ax2.set_xlabel('Epoch')
+        fig7ax2.set_ylabel('Accuracy (%)')
+        fig7ax2.legend(loc = 'upper right')
+        fig7ax2.grid(linestyle='--', linewidth=2)
+        fig7.savefig("./DoublePedulum/NNFigures/Omega Accuracy.png")
 
     def forward(self, x):
         return self.model(x)
 
 DpNNModel = DoublePendulumnNNModel()
-DpNNModel.DataGeneration(t_stop = 10, sim_step_size = 1)
+DpNNModel.DataGeneration(t_stop = 10, sim_step_size = 1, dt = 0.0005)
 DpNNModel.LossFunction()
 DpNNModel.Optimizer(optimizer = torch.optim.Adam, lr = 0.001)
-DpNNModel.Train(epochs = 200)
+DpNNModel.Train(epochs = 10)
+DpNNModel.ModelEvaluation()
 DpNNModel.DataPlots()
